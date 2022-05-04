99c1505365c39b6a7707b79ed9c22add6065dc0c
Sohail Siadatnejad <sohale@gmail.com>
2022-05-04 22:29:36 +0100

    renamed currentTraj

--- actn/action1.py
+++ actn/action1.py
@@ -31,7 +31,6 @@ target_t_sec = 0.1  # 100 msec?!
 ntimesteps = 10*2
 #ntimesteps = 20  # todo: not stable / converging to the same value when ntimesteps (discritisation segments of timespace trajectory) is increased
 #ntimesteps = 10+15 # segments of trajectory (nsteps = nsegments of time)
-# = p.xy.shape[1]
 
 (X_AXIS, Y_AXIS) = (0, 1)
 (XY_DIM, S_DIM) = (0, 1)
@@ -70,15 +69,12 @@ class Trajectory:
 
 def mutate(old_traj):
     # candidate trajectory
-    cand = Trajectory(p)
-    #print p.get_pot()
-    #print p.get_kin()
+    cand = Trajectory(old_traj)
 
     #############
     # Mutation site: @(s=j)
     ##############
-    #j = int(np.random.rand()*p.xy.shape[S_DIM])
-    j = np.random.randint(0, p.xy.shape[S_DIM])
+    j = np.random.randint(0, old_traj.xy.shape[S_DIM])
     assert j >= 0
     assert j < ntimesteps
     # Don't mutate clamped positions:
@@ -97,8 +93,7 @@ def mutate(old_traj):
     # uniform [-1,1] * σ
     #dxy = (np.random.randn(2))*PERTURB
     cand.xy[:,j] = cand.xy[:,j] + dxy
-    #print p.get_pot(), p.get_kin()
-    #print p.get_pot() - cand.get_pot(), p.get_kin() - cand.get_kin()
+    # todo: visualise (K,T), subtract (K'-K, T'-T)
 
     return cand
 
@@ -114,11 +109,10 @@ PER_HOW_MANY = 50*10
 seqoa=[]
 seqoa_i=[]
 accepted_count = 0
-p=Trajectory()
+currentTraj = Trajectory()
 # clamp end points
-p.xy[:,0]=(0.0,0.0)
-p.xy[:,-1]=(target_x_meters,target_y_meters)
-#pl.plot(p.xy[0,:],p.xy[1,:], 'b')
+currentTraj.xy[:,0] = (0.0, 0.0)
+currentTraj.xy[:,-1] = (target_x_meters, target_y_meters)
 
 MAX_COUNT = int(100000/2 * 1.4  * 10/10*3)
 # MAX_COUNT =10000 # more brief, for debug
@@ -127,21 +121,21 @@ for i in range(0,MAX_COUNT):
     sometimes = i % PER_HOW_MANY == 0
     if sometimes:
 
-        hyper_traj.append((p.xy[:,:])[None,:,:])
+        hyper_traj.append((currentTraj.xy[:,:])[None,:,:])
 
         handle = \
-        pl.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'b') #, 'color',(0.3,0.3,0.3) )
+        pl.plot(currentTraj.xy[X_AXIS,:], currentTraj.xy[Y_AXIS,:], 'b') #, 'color',(0.3,0.3,0.3) )
         handle[0].set_linewidth(0.2)
         pl.gca().set_aspect('equal')
         print(i)
         #pl.draw()
         pl.pause(0.001)
-        print( p.get_action() )
+        print( currentTraj.get_action() )
         pl.show(block=False)
 
     # candidate trajectory
 
-    cand = mutate(p)
+    cand = mutate(currentTraj)
     if cand == None:
         # skip
         continue
@@ -149,7 +143,7 @@ for i in range(0,MAX_COUNT):
     # Acceptance criteria
 
     action_new = cand.get_action()
-    da = action_new - p.get_action()
+    da = action_new - currentTraj.get_action()
     #print da
     if da > 0: # Got worse (increased). We want the least action
         continue
@@ -159,14 +153,14 @@ for i in range(0,MAX_COUNT):
     #print probr
 
     # accept the mutation
-    p = cand
+    currentTraj = cand
 
     seqoa.append(action_new)
     seqoa_i.append(i)
 
     accepted_count += 1
     if accepted_count % PER_HOW_MANY ==0:
-        print( p.get_action() )
+        print( currentTraj.get_action() )
 
 def filter1(a, alpha):
     a=np.array(a); print(a.shape)
@@ -201,9 +195,8 @@ ax2.set(xlabel='τ', ylabel=None); ax2.legend() # ax2.set_title('τ,A') # Action
 # Plot certain streaks in the overall trajectory of learning
 xyz = np.concatenate(hyper_traj,axis=0)
 print(xyz.shape) #(:, 2, ntimesteps)
-ax1.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'k')
+ax1.plot(currentTraj.xy[X_AXIS,:], currentTraj.xy[Y_AXIS,:], 'k')
 ax1.set(xlabel='x', ylabel='y') #ax1.set_title('X,Y')
-#pl.figure()
 # pl.hold(true)
 for ii in [3,5]: # out of ntimesteps
     ax1.plot(np.transpose(xyz[:,0,:]), np.transpose(xyz[:,1,:]), 'b-', linewidth=0.2)
