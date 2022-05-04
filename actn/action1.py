844379972958bc6bbda7f160cf1e545640785f22
Sohail Siadatnejad <sohale@gmail.com>
2022-05-04 18:27:27 +0100

    convert all tabs to space (using 4-space)

--- actn/action1.py
+++ actn/action1.py
@@ -25,7 +25,7 @@ _KG, _Gr = 1.0, 0.001
 _CM = 0.01
 
 # todo: xdim, ydim
-target_x_meters = 5.0 #* 2 #* 100.0 #	* 5.0 # *100
+target_x_meters = 5.0 #* 2 #* 100.0 #    * 5.0 # *100
 target_y_meters = 1.0 #* 30
 target_t_sec = 0.1  # 100 msec?!
 ntimesteps = 10
@@ -41,39 +41,39 @@ ntimesteps = 10
 # Should reach y=100
 
 class World:
-	g = 9.8 * 10000.0
+    g = 9.8 * 10000.0
 
 class Trajectory:
-	def __init__(self, trajc=None):
-		if not trajc is None:
-			self.xy = trajc.xy.copy()
-		else:
-			# initial path
-			self.xy = np.cumsum(np.random.rand(2,ntimesteps)/(float(ntimesteps) * 0.5), axis=S_DIM) * np.array([target_x_meters,target_y_meters])[:,None]
+    def __init__(self, trajc=None):
+        if not trajc is None:
+            self.xy = trajc.xy.copy()
+        else:
+            # initial path
+            self.xy = np.cumsum(np.random.rand(2,ntimesteps)/(float(ntimesteps) * 0.5), axis=S_DIM) * np.array([target_x_meters,target_y_meters])[:,None]
 
-		self.m = 1.0 * 0.0001 * _KG
-		self.dt= target_t_sec / float(ntimesteps)
+        self.m = 1.0 * 0.0001 * _KG
+        self.dt= target_t_sec / float(ntimesteps)
 
-	def get_pot(self):
-		h = self.xy[Y_AXIS,:]
-		mgh = World.g * self.m  * h
-		return np.sum(mgh) * self.dt  # integral = ∫ (mgh) dt
+    def get_pot(self):
+        h = self.xy[Y_AXIS,:]
+        mgh = World.g * self.m  * h
+        return np.sum(mgh) * self.dt  # integral = ∫ (mgh) dt
 
-	def get_kin(self):
-		# diff: piecewise
-		v = np.diff(self.xy, axis=S_DIM) / self.dt
-		mv2 = np.sum(0.5 * self.m * v * v, axis=XY_DIM)
-		return np.sum(mv2) * self.dt  # integral =  ∫ (0.5 mv^2) dt
+    def get_kin(self):
+        # diff: piecewise
+        v = np.diff(self.xy, axis=S_DIM) / self.dt
+        mv2 = np.sum(0.5 * self.m * v * v, axis=XY_DIM)
+        return np.sum(mv2) * self.dt  # integral =  ∫ (0.5 mv^2) dt
 
-	def get_action(self):
-		return self.get_kin() - self.get_pot()
+    def get_action(self):
+        return self.get_kin() - self.get_pot()
 
 def mutate(old_traj):
 
-	return cand
+    return cand
 
 def accept_mutation(cand, old_traj):
-	return
+    return
 
 # hyper/meta trajectory
 hyper_traj = []
@@ -95,78 +95,78 @@ MAX_COUNT = int(100000/2 * 1.4) * 10
 # MAX_COUNT =10000 # more brief, for debug
 
 for i in range(0,MAX_COUNT):
-	sometimes = i % PER_HOW_MANY == 0
-	if sometimes:
+    sometimes = i % PER_HOW_MANY == 0
+    if sometimes:
 
-		hyper_traj.append((p.xy[:,:])[None,:,:])
+        hyper_traj.append((p.xy[:,:])[None,:,:])
 
-		handle = \
-		pl.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'b') #, 'color',(0.3,0.3,0.3) )
-		handle[0].set_linewidth(0.2)
-		pl.gca().set_aspect('equal')
-		print(i)
-		#pl.draw()
-		pl.pause(0.001)
-		print( p.get_action() )
-		pl.show(block=False)
+        handle = \
+        pl.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'b') #, 'color',(0.3,0.3,0.3) )
+        handle[0].set_linewidth(0.2)
+        pl.gca().set_aspect('equal')
+        print(i)
+        #pl.draw()
+        pl.pause(0.001)
+        print( p.get_action() )
+        pl.show(block=False)
 
   # candidate trajectory
-	cand = Trajectory(p)
-	#print p.get_pot()
-	#print p.get_kin()
+    cand = Trajectory(p)
+    #print p.get_pot()
+    #print p.get_kin()
 
-	# Mutation site: @(s=j)
-	#j = int(np.random.rand()*p.xy.shape[S_DIM])
-	j = np.random.randint(0, p.xy.shape[S_DIM])
-	assert j >= 0
-	assert j < ntimesteps
-	# Don't mutate clamped positions:
-	if j == 0:
-		#return None
-		continue
-	if j == cand.xy.shape[1]-1:
-		#return None
-		continue
+    # Mutation site: @(s=j)
+    #j = int(np.random.rand()*p.xy.shape[S_DIM])
+    j = np.random.randint(0, p.xy.shape[S_DIM])
+    assert j >= 0
+    assert j < ntimesteps
+    # Don't mutate clamped positions:
+    if j == 0:
+        #return None
+        continue
+    if j == cand.xy.shape[1]-1:
+        #return None
+        continue
 
-	#if cand == None:
-	#	continue
+    #if cand == None:
+    #    continue
 
-	##############
-	# Mutation
-	##############
-	PERTURB = 0.1
-	# Normal(0,σ)
-	dxy = (np.random.rand(2)*2-1.0)*PERTURB
-	# uniform [-1,1] * σ
-	#dxy = (np.random.randn(2))*PERTURB
-	cand.xy[:,j] = cand.xy[:,j] + dxy
-	#print p.get_pot(), p.get_kin()
-	#print p.get_pot() - cand.get_pot(), p.get_kin() - cand.get_kin()
+    ##############
+    # Mutation
+    ##############
+    PERTURB = 0.1
+    # Normal(0,σ)
+    dxy = (np.random.rand(2)*2-1.0)*PERTURB
+    # uniform [-1,1] * σ
+    #dxy = (np.random.randn(2))*PERTURB
+    cand.xy[:,j] = cand.xy[:,j] + dxy
+    #print p.get_pot(), p.get_kin()
+    #print p.get_pot() - cand.get_pot(), p.get_kin() - cand.get_kin()
 
-	# Acceptance criteria
+    # Acceptance criteria
 
-	a=cand.get_action()
-	da= a - p.get_action()
-	#print da
-	if da>0: #worse
-		continue
-	#Temp = 100.0
-	#probr = math.exp( -abs(da)/Temp )
-	#print probr
-	#seqa.append(a)
+    a=cand.get_action()
+    da= a - p.get_action()
+    #print da
+    if da>0: #worse
+        continue
+    #Temp = 100.0
+    #probr = math.exp( -abs(da)/Temp )
+    #print probr
+    #seqa.append(a)
 
-	# accept the mutation
-	p = cand
+    # accept the mutation
+    p = cand
 
-	alpha=0.01
-	slowda = slowda * (1.0-alpha) + a * (alpha)
-	seqa.append(slowda)
-	seqoa.append(a)
+    alpha=0.01
+    slowda = slowda * (1.0-alpha) + a * (alpha)
+    seqa.append(slowda)
+    seqoa.append(a)
 
-	accepted_count += 1
-	if accepted_count % PER_HOW_MANY ==0:
-		print( p.get_action() )
-	#print( p.get_action() )
+    accepted_count += 1
+    if accepted_count % PER_HOW_MANY ==0:
+        print( p.get_action() )
+    #print( p.get_action() )
 
 pl.figure()
 # What is this figure showing?
