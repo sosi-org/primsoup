acc65157b7c565f810584a5ce8e9da65e198f874
Sohail Siadatnejad <sohale@gmail.com>
2022-05-04 17:38:22 +0100

    cute animation growing up

--- actn/action1.py
+++ actn/action1.py
@@ -21,13 +21,20 @@ import numpy as np
 import math
 import matplotlib.pylab as pl
 
-# todo: xdim, ydim
-xlen = 5.0 #* 2 #* 100.0 #	* 5.0 # *100
-ylen = 1.0 #* 30
-#ntimesteps = 10
-#ntimesteps = 20  # todo: not stable / converging to the same value when ntimesteps (discritisation segments of timespace trajectory) is increased
-ntimesteps = 10+15 # segments of trajectory (nsteps = nsegments of time)
+_KG, _Gr = 1.0, 0.001
+_CM = 0.01
 
+# todo: xdim, ydim
+target_x_meters = 5.0 #* 2 #* 100.0 #	* 5.0 # *100
+target_y_meters = 1.0 #* 30
+target_t_sec = 0.1  # 100 msec?!
+ntimesteps = 10
+#ntimesteps = 20  # todo: not stable / converging to the same value when ntimesteps (discritisation segments of timespace trajectory) is increased
+#ntimesteps = 10+15 # segments of trajectory (nsteps = nsegments of time)
+# = p.xy.shape[1]
+
+X_AXIS = 0
+Y_AXIS = 1
 # Should reach y=100
 
 class Path:
@@ -40,15 +47,15 @@ class Path:
 		else:
 			#self.xy = np.zeros((2,ntimesteps))
 			# initial path
-			self.xy = np.cumsum(np.random.rand(2,ntimesteps)/(float(ntimesteps) * 0.5), axis=1) * np.array([xlen,ylen])[:,None]
+			self.xy = np.cumsum(np.random.rand(2,ntimesteps)/(float(ntimesteps) * 0.5), axis=1) * np.array([target_x_meters,target_y_meters])[:,None]
 			print(self.xy)
 
 		#self.A=np.zeros((2,))
 		#self.B=np.zeros((2,))
-		self.m = 1.0
+		self.m = 1.0 * _KG
 		#self.m_segm = self._m / float(ntimesteps) # mass per segment
 		#self.t = ?
-		self.dt= 0.01 * 10.0 / float(ntimesteps)
+		self.dt= target_t_sec / float(ntimesteps)
 
 
 	def piecewise(self):
@@ -56,11 +63,11 @@ class Path:
 		return np.diff(self.xy, axis=1)
 
 	def get_pot(self):
-		return np.sum(self.g * self.m * self.xy[1,:]) * self.dt
+		return np.sum(self.g * self.m * self.xy[Y_AXIS,:]) * self.dt
 
 	def get_kin(self):
-		v = self.piecewise()
-		xy=np.sum(0.5 * self.m * v * v, axis=1) * self.dt
+		v = self.piecewise() / self.dt * 0.01
+		xy=np.sum(0.5 * self.m * v * v, axis=0) * self.dt
 		return np.sum(xy) #x+y
 
 	def get_action(self):
@@ -79,7 +86,7 @@ ctr=0
 p=Path()
 # clamp end points
 p.xy[:,0]=(0.0,0.0)
-p.xy[:,-1]=(xlen,ylen)
+p.xy[:,-1]=(target_x_meters,target_y_meters)
 #pl.plot(p.xy[0,:],p.xy[1,:], 'b')
 
 MAX_COUNT = int(100000/2 * 1.4) * 10
@@ -88,11 +95,12 @@ MAX_COUNT = int(100000/2 * 1.4) * 10
 for i in range(0,MAX_COUNT):
 	if i % PER_HOW_MANY ==0:
 
-		hyper_traj.append((p.xy[:2,:])[None,:,:])
+		hyper_traj.append((p.xy[:,:])[None,:,:])
 
 		handle = \
-		pl.plot(p.xy[0,:],p.xy[1,:], 'b') #, 'color',(0.3,0.3,0.3) )
+		pl.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'b') #, 'color',(0.3,0.3,0.3) )
 		handle[0].set_linewidth(0.2)
+		pl.gca().set_aspect('equal')
 		print(i)
 		#pl.draw()
 		pl.pause(0.001)
@@ -144,8 +152,9 @@ for i in range(0,MAX_COUNT):
 
 pl.figure()
 # What is this figure showing?
-DT=0.01
-pl.plot(p.xy[0,:],p.xy[1,:], 'k')
+#DT=0.01
+DT=p.dt # sec
+pl.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'k')
 ta=np.arange(0.0,float(len(seqa)))/float(len(seqa))
 pl.plot(ta,np.array(seqoa),'r')
 pl.plot(ta[1:],np.diff(seqa)/DT *10)  # dx/dt
