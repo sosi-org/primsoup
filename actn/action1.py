acdcbaca450d346c049a0c656f178c258af9fc9f
Sohail Siadatnejad <sohale@gmail.com>
2022-05-04 21:12:10 +0100

    fantastic log-scale x

--- actn/action1.py
+++ actn/action1.py
@@ -120,7 +120,7 @@ p.xy[:,0]=(0.0,0.0)
 p.xy[:,-1]=(target_x_meters,target_y_meters)
 #pl.plot(p.xy[0,:],p.xy[1,:], 'b')
 
-MAX_COUNT = int(100000/2 * 1.4  * 10/10/5)
+MAX_COUNT = int(100000/2 * 1.4  * 10/10)
 # MAX_COUNT =10000 # more brief, for debug
 
 for i in range(0,MAX_COUNT):
@@ -161,7 +161,6 @@ for i in range(0,MAX_COUNT):
     # accept the mutation
     p = cand
 
-    alpha=0.01
     seqoa.append(action_new)
     seqoa_i.append(i)
 
@@ -196,9 +195,9 @@ ax1.plot(p.xy[X_AXIS,:],p.xy[Y_AXIS,:], 'k')
 ax1.set(xlabel='x', ylabel='y') #ax1.set_title('X,Y')
 
 ax2.plot(ta,np.array(seqoa),'r', label='A')
-ax2.plot(ta[1:],np.diff(np.array(seqoa))*1000,'k.', label='ΔA')
+ax2.plot(ta[1:],np.diff(np.array(seqoa))*1000, 'k.', markersize=0.2, label='ΔA')
 ax2.plot(ta[1:],np.diff(filter1(seqoa, 0.01))/DT*1000, 'b', label='dA')  # dx/dt
-
+ax2.set_xscale('log')
 ax2.set(xlabel='τ', ylabel=None); ax2.legend() # ax2.set_title('τ,A') # Action
 
 # Plot certain streaks in the overall trajectory of learning
